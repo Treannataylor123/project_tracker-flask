9d0f8a2d2bc90555e65b2bc4aca0446845f60288
Your Name <you@example.com>
2020-02-20 14:49:49 -0800

    in progress

--- server.py
+++ server.py
@@ -10,11 +10,49 @@ app = Flask(__name__)
 def get_student():
     """Show information about a student."""
 
-    github = 'jhacks'
+    github = request.args.get('github')
 
     first, last, github = hackbright.get_student_by_github(github)
 
-    return '{github} is the GitHub account for {first} {last}'
+    grades = hackbright.get_grades_by_github(github)
+
+
+    return render_template("student_info.html", github_user=github,
+                           first_name=first, last_name=last, grades=grades)
+
+
+@app.route("/search")
+def search_student():
+    """search user names"""
+
+    return render_template("student_search.html")
+
+
+@app.route("/new_student")
+def new_student():
+    """" Displaying new student input form"""
+    return render_template("add_student.html")
+
+
+@app.route("/add_student", methods=['POST'])
+def add_student():
+    """Adding a student to database"""
+
+    last_name = request.form.get('last_name')
+    first_name = request.form.get('first_name')
+    github = request.form.get('github')
+
+    output = hackbright.make_new_student(first_name, last_name, github)
+
+    return f"{output}"
+
+
+@app.route("student/<project>")
+def display_project():
+    """Display information about given project."""
+
+    return render_template("project_info.html")
+
 
 
 if __name__ == '__main__':
